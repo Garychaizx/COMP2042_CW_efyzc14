13e9a3ab5d3efc5906305e5ef121479b24139bd3
Garychaizx <zxuan8103@gmail.com>
2023-11-12 15:10:15 +0800

    - Introduce constants for animation steps and delay - Encapsulate label creation and animation into private methods - Utilize lambda expressions for concise event handling

--- src/main/java/brickGame/Score.java
+++ src/main/java/brickGame/Score.java
@@ -1,117 +1,87 @@
 package brickGame;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-//import sun.plugin2.message.Message;
+import javafx.util.Duration;
+
+import java.util.function.IntUnaryOperator;
 
 public class Score {
     public void show(final double x, final double y, int score, final Main main) {
-        String sign;
-        if (score >= 0) {
-            sign = "+";
-        } else {
-            sign = "";
-        }
-        final Label label = new Label(sign + score);
-        label.setTranslateX(x);
-        label.setTranslateY(y);
+        String sign = (score >= 0) ? "+" : "";
+        final Label label = createLabel(sign + score, x, y);
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 21; i++) {
-                    try {
-                        label.setScaleX(i);
-                        label.setScaleY(i);
-                        label.setOpacity((20 - i) / 20.0);
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        Platform.runLater(() -> main.root.getChildren().add(label));
+        animateLabel(label, 21, i -> i, 20);
     }
 
     public void showMessage(String message, final Main main) {
-        final Label label = new Label(message);
-        label.setTranslateX(220);
-        label.setTranslateY(340);
+        final Label label = createLabel(message, 220, 340);
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 21; i++) {
-                    try {
-                        label.setScaleX(Math.abs(i-10));
-                        label.setScaleY(Math.abs(i-10));
-                        label.setOpacity((20 - i) / 20.0);
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        Platform.runLater(() -> main.root.getChildren().add(label));
+        animateLabel(label, 21, i -> Math.abs(i - 10), 20);
     }
 
     public void showGameOver(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("Game Over :(");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
+        Platform.runLater(() -> {
+            Label label = createLabel("Game Over :(", 200, 250);
+            label.setScaleX(2);
+            label.setScaleY(2);
 
-                Button restart = new Button("Restart");
-                restart.setTranslateX(220);
-                restart.setTranslateY(300);
-                restart.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        main.restartGame();
-                    }
-                });
+            Button restart = createRestartButton(main);
 
-                main.root.getChildren().addAll(label, restart);
-
-            }
+            main.root.getChildren().addAll(label, restart);
         });
     }
 
     public void showWin(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("You Win :)");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
+        Platform.runLater(() -> {
+            Label label = createLabel("You Win :)", 200, 250);
+            label.setScaleX(2);
+            label.setScaleY(2);
 
-
-                main.root.getChildren().addAll(label);
-
-            }
+            main.root.getChildren().addAll(label);
         });
     }
+
+    private Label createLabel(String text, double x, double y) {
+        Label label = new Label(text);
+        label.setTranslateX(x);
+        label.setTranslateY(y);
+        return label;
+    }
+
+    private Button createRestartButton(Main main) {
+        Button restart = new Button("Restart");
+        restart.setTranslateX(220);
+        restart.setTranslateY(300);
+        restart.setOnAction(event -> main.restartGame());
+        return restart;
+    }
+
+    private void animateLabel(Label label, int iterations, IntUnaryOperator scaleXFunction, int opacityBase) {
+        Timeline timeline = new Timeline();
+
+        for (int i = 0; i < iterations; i++) {
+
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(i * 15),
+                    new KeyValue(label.scaleXProperty(), scaleXFunction.applyAsInt(i)),
+                    new KeyValue(label.scaleYProperty(), scaleXFunction.applyAsInt(i)),
+                    new KeyValue(label.opacityProperty(), (opacityBase - i) / (double) opacityBase)
+            );
+            timeline.getKeyFrames().add(keyFrame);
+        }
+
+        timeline.play();
+    }
+
+
 }
+
+
+
